3cc4240572efb0375163f40325eec3c9d9166159
NEFORCEO <pirogartem919@gmail.com>
2026-02-03 22:30:07 +0700

    Adds example to fetch IP address
    Adds a new example that demonstrates how to use
    fasthttp to fetch the IP address from a remote
    service. This showcases a simple GET request
    and JSON response parsing.

---
+++ examples/get_ip.py
@@ -0,0 +1,13 @@
+from fasthttp import FastHTTP
+from fasthttp.response import Response
+
+app = FastHTTP(debug=True)
+
+
+@app.get(url="https://httpbin.org/ip")
+async def get_ip(resp: Response):
+    return resp.json()
+
+
+if __name__ == "__main__":
+    app.run()
