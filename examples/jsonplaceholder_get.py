2a1b68a160f79958ecb3e613b07ff6eff306f133
NEFORCEO <pirogartem919@gmail.com>
2026-02-03 22:32:20 +0700

    Adds jsonplaceholder GET example
    Adds an example demonstrating how to perform a GET request to the JSONPlaceholder API.
    
    This showcases the usage of the `FastHTTP` client to fetch data from an external API and parse the response as JSON.

---
+++ examples/jsonplaceholder_get.py
@@ -0,0 +1,13 @@
+from fasthttp import FastHTTP
+from fasthttp.response import Response
+
+app = FastHTTP(debug=True)
+
+
+@app.get(url="https://jsonplaceholder.typicode.com/posts/1")
+async def jsonplaceholder_get(resp: Response):
+    return resp.json()
+
+
+if __name__ == "__main__":
+    app.run()
