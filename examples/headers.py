115b0971f7abcb801d45e0089704cc6bd96709d1
NEFORCEO <pirogartem919@gmail.com>
2026-02-03 22:30:59 +0700

    Adds headers example
    Adds an example demonstrating how to fetch and parse headers from an external API endpoint using the library.
    
    This example showcases a simple get request to "https://httpbin.org/headers" and returns the response as a JSON object.

---
+++ examples/headers.py
@@ -0,0 +1,12 @@
+from fasthttp import FastHTTP
+from fasthttp.response import Response
+
+app = FastHTTP(debug=True)
+
+
+@app.get(url="https://httpbin.org/headers")
+async def headers(resp: Response):
+    return resp.json()
+
+if __name__ == "__main__":
+    app.run()
