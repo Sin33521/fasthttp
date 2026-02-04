ceb4450c9bf36622438d19dbff12ddbcfdf26237
NEFORCEO <pirogartem919@gmail.com>
2026-02-04 18:04:12 +0700

    Adds example for handling not found site
    Adds an example demonstrating how to handle a "not found" site scenario using the FastHTTP framework.
    
    The example defines a route that attempts to access a potentially non-existent URL and returns the response status.

---
+++ examples/error/not_found_site.py
@@ -0,0 +1,12 @@
+from fasthttp import FastHTTP
+from fasthttp.response import Response
+
+app = FastHTTP(debug=True)
+
+
+@app.get(url="https:///niwqGFDQUBCIBciwbecibecc.cloud")
+async def test_site(r: Response):
+    return r.status
+
+if __name__ == "__main__":
+    app.run()
