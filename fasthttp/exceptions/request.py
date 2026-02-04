9221df827b16df44e7d0b8b9df567485ca5b1514
NEFORCEO <pirogartem919@gmail.com>
2026-02-04 20:58:21 +0700

    Adds FastHTTPRequestError exception
    Introduces a new exception, `FastHTTPRequestError`, to handle errors occurring during request preparation or execution.
    
    This exception provides detailed information about the error, including the URL and HTTP method involved.

---
+++ fasthttp/exceptions/request.py
@@ -0,0 +1,58 @@
+from typing import Annotated
+
+from annotated_doc import Doc
+
+from .base import FastHTTPError
+
+
+class FastHTTPRequestError(FastHTTPError):
+    """
+    Raised when request preparation or execution fails.
+
+    Example:
+        >>> raise FastHTTPRequestError
+        (
+            "Invalid URL",
+            url="invalid-url"
+        )
+    """
+
+    def __init__(
+        self,
+        message: Annotated[
+            str,
+            Doc(
+                """
+                Description of the request-level error.
+
+                Used when request preparation or execution
+                fails before receiving a response
+                (e.g. invalid URL, invalid parameters).
+                """
+            )],
+        url: Annotated[
+            str,
+            Doc(
+                """
+                Target request URL that caused the error.
+
+                May be invalid, malformed or unsupported.
+                """
+            )] | None = None,
+        method: Annotated[
+            str,
+            Doc(
+                """
+                HTTP method that was being used
+
+                when the request error occurred.
+                """
+            )] | None = None,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            message=message,
+            url=url,
+            method=method,
+            **kwargs,
+        )
